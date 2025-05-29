4b30cb41b0ac2fe583c1cd97bdee6bef58c9e046
douglashenriquemci <douglas.henrique@wearemci.com>
2025-05-29 14:40:09 -0300

    navigation changes and global css

--- template_parts/navigation.php
+++ template_parts/navigation.php
@@ -1,92 +1,157 @@
 <div class='nav_wrap'>
 	<ul class='steps clearfix'>
 		
-		<!-- 
-			Class on li: "active" for selected state, "done" for finished state. No class for future state.
-		 -->
+		<!-- Step 1 -->
 		<li class='label active' data-navigation-step='1'>
-			<div class='circle_wrap'>
-				<span class='circle_outer'>
-					<span class='circle_inner'></span>
+			<div class='circle_text_row' style="display: flex; align-items: center; gap: 8px;">
+				<!-- Active SVG -->
+				<span class='circle_svg_active'>
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM10.0001 13.3333C10.8841 13.3333 11.732 12.9822 12.3571 12.357C12.9822 11.7319 13.3334 10.8841 13.3334 10C13.3334 9.11595 12.9822 8.2681 12.3571 7.64298C11.732 7.01786 10.8841 6.66667 10.0001 6.66667C9.11603 6.66667 8.26818 7.01786 7.64306 7.64298C7.01794 8.2681 6.66675 9.11595 6.66675 10C6.66675 10.8841 7.01794 11.7319 7.64306 12.357C8.26818 12.9822 9.11603 13.3333 10.0001 13.3333Z" fill="#9A8142"/>
+					</svg>
 				</span>
-			</div>
-			<p>
-				<a href='#' data-navigation-step-link='1' data-language='navigation__step1' data-language-original="Enter Information">
-					Enter Information
+				<!-- Completed SVG -->
+				<span class='circle_svg_completed'>
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  					<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM9.16925 13.3333L15.0609 7.44084L13.8826 6.26251L9.16925 10.9767L6.81175 8.61917L5.63341 9.79751L9.16925 13.3333Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<!-- Inactive SVG -->
+				<span class='circle_svg_inactive'>
+					<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.5001 18.3333C5.89758 18.3333 2.16675 14.6025 2.16675 10C2.16675 5.39751 5.89758 1.66667 10.5001 1.66667C15.1026 1.66667 18.8334 5.39751 18.8334 10C18.8334 14.6025 15.1026 18.3333 10.5001 18.3333ZM10.5001 16.6667C12.2682 16.6667 13.9639 15.9643 15.2141 14.7141C16.4644 13.4638 17.1667 11.7681 17.1667 10C17.1667 8.2319 16.4644 6.5362 15.2141 5.28596C13.9639 4.03572 12.2682 3.33334 10.5001 3.33334C8.73197 3.33334 7.03628 4.03572 5.78604 5.28596C4.53579 6.5362 3.83341 8.2319 3.83341 10C3.83341 11.7681 4.53579 13.4638 5.78604 14.7141C7.03628 15.9643 8.73197 16.6667 10.5001 16.6667Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<a href='#' data-navigation-step-link='1' data-language='navigation__step1' data-language-original="Enter Information" class="step-label-text active">
+					1. Enter Information
 				</a>
-			</p>
+			</div>
 		</li>
 		
-		<li class='connector' data-navigation-step-connector='1'>
-		</li>
+		<li class='connector' data-navigation-step-connector='1'></li>
 		
+		<!-- Step 2 -->
 		<li class='label' data-navigation-step='2'>
-			<div class='circle_wrap'>
-				<span class='circle_outer'>
-					<span class='circle_inner'></span>
+			<div class='circle_text_row' style="display: flex; align-items: center; gap: 8px;">
+				<!-- Active SVG -->
+				<span class='circle_svg_active' style="display:none; align-items: center; justify-content: center; width: 20px; height: 20px;">
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM10.0001 13.3333C10.8841 13.3333 11.732 12.9822 12.3571 12.357C12.9822 11.7319 13.3334 10.8841 13.3334 10C13.3334 9.11595 12.9822 8.2681 12.3571 7.64298C11.732 7.01786 10.8841 6.66667 10.0001 6.66667C9.11603 6.66667 8.26818 7.01786 7.64306 7.64298C7.01794 8.2681 6.66675 9.11595 6.66675 10C6.66675 10.8841 7.01794 11.7319 7.64306 12.357C8.26818 12.9822 9.11603 13.3333 10.0001 13.3333Z" fill="#9A8142"/>
+					</svg>
 				</span>
-			</div>
-			<p>
-				<a href='#' data-navigation-step-link='2' data-language='navigation__step2' data-language-original="Design Suite">
-					Design Suite
+				<!-- Completed SVG -->
+				<span class='circle_svg_completed'>
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  					<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM9.16925 13.3333L15.0609 7.44084L13.8826 6.26251L9.16925 10.9767L6.81175 8.61917L5.63341 9.79751L9.16925 13.3333Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<!-- Inactive SVG -->
+				<span class='circle_svg_inactive' style="display:flex; align-items: center; justify-content: center; width: 21px; height: 20px;">
+					<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.5001 18.3333C5.89758 18.3333 2.16675 14.6025 2.16675 10C2.16675 5.39751 5.89758 1.66667 10.5001 1.66667C15.1026 1.66667 18.8334 5.39751 18.8334 10C18.8334 14.6025 15.1026 18.3333 10.5001 18.3333ZM10.5001 16.6667C12.2682 16.6667 13.9639 15.9643 15.2141 14.7141C16.4644 13.4638 17.1667 11.7681 17.1667 10C17.1667 8.2319 16.4644 6.5362 15.2141 5.28596C13.9639 4.03572 12.2682 3.33334 10.5001 3.33334C8.73197 3.33334 7.03628 4.03572 5.78604 5.28596C4.53579 6.5362 3.83341 8.2319 3.83341 10C3.83341 11.7681 4.53579 13.4638 5.78604 14.7141C7.03628 15.9643 8.73197 16.6667 10.5001 16.6667Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<a href='#' data-navigation-step-link='2' data-language='navigation__step2' data-language-original="Design Suite" class="step-label-text">
+					2. Design Suite
 				</a>
-			</p>
+			</div>
 		</li>
 		
-		<li class='connector' data-navigation-step-connector='2'>
-		</li>
+		<li class='connector' data-navigation-step-connector='2'></li>
 		
+		<!-- Step 3 -->
 		<li class='label' data-navigation-step='3'>
-			<div class='circle_wrap'>
-				<span class='circle_outer'>
-					<span class='circle_inner'></span>
+			<div class='circle_text_row' style="display: flex; align-items: center; gap: 8px;">
+				<!-- Active SVG -->
+				<span class='circle_svg_active' style="display:none; align-items: center; justify-content: center; width: 20px; height: 20px;">
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM10.0001 13.3333C10.8841 13.3333 11.732 12.9822 12.3571 12.357C12.9822 11.7319 13.3334 10.8841 13.3334 10C13.3334 9.11595 12.9822 8.2681 12.3571 7.64298C11.732 7.01786 10.8841 6.66667 10.0001 6.66667C9.11603 6.66667 8.26818 7.01786 7.64306 7.64298C7.01794 8.2681 6.66675 9.11595 6.66675 10C6.66675 10.8841 7.01794 11.7319 7.64306 12.357C8.26818 12.9822 9.11603 13.3333 10.0001 13.3333Z" fill="#9A8142"/>
+					</svg>
 				</span>
-			</div>
-			<p>
-				<a href='#' data-navigation-step-link='3' data-language='navigation__step3' data-language-original="Edit Suite">
-					Edit Suite
+				<!-- Completed SVG -->
+				<span class='circle_svg_completed'>
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  					<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM9.16925 13.3333L15.0609 7.44084L13.8826 6.26251L9.16925 10.9767L6.81175 8.61917L5.63341 9.79751L9.16925 13.3333Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<!-- Inactive SVG -->
+				<span class='circle_svg_inactive' style="display:flex; align-items: center; justify-content: center; width: 21px; height: 20px;">
+					<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.5001 18.3333C5.89758 18.3333 2.16675 14.6025 2.16675 10C2.16675 5.39751 5.89758 1.66667 10.5001 1.66667C15.1026 1.66667 18.8334 5.39751 18.8334 10C18.8334 14.6025 15.1026 18.3333 10.5001 18.3333ZM10.5001 16.6667C12.2682 16.6667 13.9639 15.9643 15.2141 14.7141C16.4644 13.4638 17.1667 11.7681 17.1667 10C17.1667 8.2319 16.4644 6.5362 15.2141 5.28596C13.9639 4.03572 12.2682 3.33334 10.5001 3.33334C8.73197 3.33334 7.03628 4.03572 5.78604 5.28596C4.53579 6.5362 3.83341 8.2319 3.83341 10C3.83341 11.7681 4.53579 13.4638 5.78604 14.7141C7.03628 15.9643 8.73197 16.6667 10.5001 16.6667Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<a href='#' data-navigation-step-link='3' data-language='navigation__step3' data-language-original="Edit Suite" class="step-label-text">
+					3. Edit Suite
 				</a>
-			</p>
+			</div>
 		</li>
 		
-		<li class='connector' data-navigation-step-connector='3'>
-		</li>
+		<li class='connector' data-navigation-step-connector='3'></li>
 		
+		<!-- Step 4 -->
 		<li class='label' data-navigation-step='4'>
-			<div class='circle_wrap'>
-				<span class='circle_outer'>
-					<span class='circle_inner'></span>
+			<div class='circle_text_row' style="display: flex; align-items: center; gap: 8px;">
+				<!-- Active SVG -->
+				<span class='circle_svg_active' style="display:none; align-items: center; justify-content: center; width: 20px; height: 20px;">
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM10.0001 13.3333C10.8841 13.3333 11.732 12.9822 12.3571 12.357C12.9822 11.7319 13.3334 10.8841 13.3334 10C13.3334 9.11595 12.9822 8.2681 12.3571 7.64298C11.732 7.01786 10.8841 6.66667 10.0001 6.66667C9.11603 6.66667 8.26818 7.01786 7.64306 7.64298C7.01794 8.2681 6.66675 9.11595 6.66675 10C6.66675 10.8841 7.01794 11.7319 7.64306 12.357C8.26818 12.9822 9.11603 13.3333 10.0001 13.3333Z" fill="#9A8142"/>
+					</svg>
 				</span>
-			</div>
-			<p>
-				<a href='#' data-navigation-step-link='4' data-language='navigation__step4' data-language-original="Visualize 3D">
-					Visualize 3D
+				<!-- Completed SVG -->
+				<span class='circle_svg_completed'>
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  					<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM9.16925 13.3333L15.0609 7.44084L13.8826 6.26251L9.16925 10.9767L6.81175 8.61917L5.63341 9.79751L9.16925 13.3333Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<!-- Inactive SVG -->
+				<span class='circle_svg_inactive' style="display:flex; align-items: center; justify-content: center; width: 21px; height: 20px;">
+					<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.5001 18.3333C5.89758 18.3333 2.16675 14.6025 2.16675 10C2.16675 5.39751 5.89758 1.66667 10.5001 1.66667C15.1026 1.66667 18.8334 5.39751 18.8334 10C18.8334 14.6025 15.1026 18.3333 10.5001 18.3333ZM10.5001 16.6667C12.2682 16.6667 13.9639 15.9643 15.2141 14.7141C16.4644 13.4638 17.1667 11.7681 17.1667 10C17.1667 8.2319 16.4644 6.5362 15.2141 5.28596C13.9639 4.03572 12.2682 3.33334 10.5001 3.33334C8.73197 3.33334 7.03628 4.03572 5.78604 5.28596C4.53579 6.5362 3.83341 8.2319 3.83341 10C3.83341 11.7681 4.53579 13.4638 5.78604 14.7141C7.03628 15.9643 8.73197 16.6667 10.5001 16.6667Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<a href='#' data-navigation-step-link='4' data-language='navigation__step4' data-language-original="Visualize 3D" class="step-label-text">
+					4. Visualize 3D
 				</a>
-			</p>
+			</div>
 		</li>
 		
-		<li class='connector' data-navigation-step-connector='4'>
-		</li>
+		<li class='connector' data-navigation-step-connector='4'></li>
 		
+		<!-- Step 5 -->
 		<li class='label' data-navigation-step='5'>
-			<div class='circle_wrap'>
-				<span class='circle_outer'>
-					<span class='circle_inner'></span>
+			<div class='circle_text_row' style="display: flex; align-items: center; gap: 8px;">
+				<!-- Active SVG -->
+				<span class='circle_svg_active' style="display:none; align-items: center; justify-content: center; width: 20px; height: 20px;">
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM10.0001 13.3333C10.8841 13.3333 11.732 12.9822 12.3571 12.357C12.9822 11.7319 13.3334 10.8841 13.3334 10C13.3334 9.11595 12.9822 8.2681 12.3571 7.64298C11.732 7.01786 10.8841 6.66667 10.0001 6.66667C9.11603 6.66667 8.26818 7.01786 7.64306 7.64298C7.01794 8.2681 6.66675 9.11595 6.66675 10C6.66675 10.8841 7.01794 11.7319 7.64306 12.357C8.26818 12.9822 9.11603 13.3333 10.0001 13.3333Z" fill="#9A8142"/>
+					</svg>
 				</span>
-			</div>
-			<p>
-				<a href='#' data-navigation-step-link='5' data-language='navigation__step5' data-language-original="See Outcome">
-					See Outcome
+				<!-- Completed SVG -->
+				<span class='circle_svg_completed'>
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  					<path d="M10.0001 18.3333C5.39758 18.3333 1.66675 14.6025 1.66675 10C1.66675 5.39751 5.39758 1.66667 10.0001 1.66667C14.6026 1.66667 18.3334 5.39751 18.3334 10C18.3334 14.6025 14.6026 18.3333 10.0001 18.3333ZM9.16925 13.3333L15.0609 7.44084L13.8826 6.26251L9.16925 10.9767L6.81175 8.61917L5.63341 9.79751L9.16925 13.3333Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<!-- Inactive SVG -->
+				<span class='circle_svg_inactive' style="display:flex; align-items: center; justify-content: center; width: 21px; height: 20px;">
+					<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M10.5001 18.3333C5.89758 18.3333 2.16675 14.6025 2.16675 10C2.16675 5.39751 5.89758 1.66667 10.5001 1.66667C15.1026 1.66667 18.8334 5.39751 18.8334 10C18.8334 14.6025 15.1026 18.3333 10.5001 18.3333ZM10.5001 16.6667C12.2682 16.6667 13.9639 15.9643 15.2141 14.7141C16.4644 13.4638 17.1667 11.7681 17.1667 10C17.1667 8.2319 16.4644 6.5362 15.2141 5.28596C13.9639 4.03572 12.2682 3.33334 10.5001 3.33334C8.73197 3.33334 7.03628 4.03572 5.78604 5.28596C4.53579 6.5362 3.83341 8.2319 3.83341 10C3.83341 11.7681 4.53579 13.4638 5.78604 14.7141C7.03628 15.9643 8.73197 16.6667 10.5001 16.6667Z" fill="#9A8142"/>
+					</svg>
+				</span>
+				<a href='#' data-navigation-step-link='5' data-language='navigation__step5' data-language-original="See Outcome" class="step-label-text">
+					5. See Outcome
 				</a>
-			</p>
+			</div>
 		</li>
 		
 	</ul>
 </div>
 	
+<!--
 <button class='secondary_button nav_previous' id='nav_previous' disabled>
-	&lt; <span data-language='navigation__previous' data-language-original="Previous">Previous</span>
-</button>	
-<button class='secondary_button nav_next' id='nav_next' disabled>
-	&gt; <span data-language='navigation__next' data-language-original="Next">Next</span>
+    &lt; <span data-language='navigation__previous' data-language-original="Previous">Previous</span>
 </button>
+-->
+<!-- <button class='secondary_button nav_next' id='nav_next' disabled>
+	&gt; <span data-language='navigation__next' data-language-original="Next">Next</span>
+</button> -->
