cb7ddab2cf8a8e1f8dba2a6f4fdcc369fb9b4475
flipzinho <felipe.ribeiro@wearemci.com>
2025-06-02 18:17:12 -0300

    navigation

--- template_parts/step_3d.php
+++ template_parts/step_3d.php
@@ -4,10 +4,13 @@
 <div class='appArea step_3d' id='step3DArea'>
 	<div class='bottom_buttons'>
 		<button class='primary_button bottom_button' id='step4PreviousButton'>
-			 <span data-language='step_4__previous_button' data-language-original="Previous">Previous</span>
+			 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M6.52325 9.16664H16.6666V10.8333H6.52325L10.9933 15.3033L9.81492 16.4816L3.33325 9.99998L9.81492 3.51831L10.9933 4.69664L6.52325 9.16664Z" fill="white"></path>
+			</svg> <span data-language='step_4__previous_button' data-language-original="Previous">Previous</span>
 		</button>
+		
 		<button class='primary_button bottom_button' id='step4NextButton'>
-			<span data-language='step_4__next_button' data-language-original="Next">Next</span> 
+			<span data-language='step_4__next_button' data-language-original="Next Step">Next Step</span><img src="assets/images/arrow-right-line.png" alt="" width="20" height="20"> 
 		</button>
 	</div>
 </div>
